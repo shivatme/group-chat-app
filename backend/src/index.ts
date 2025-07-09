925f33df403c88914a05ca11cb29ccbe635a7cda
Shivam5278 <st527874@gmail.com>
2025-07-09 17:00:25 +0530

    Init mobile.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { createServer } from "./server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3005;
+const HOST = "0.0.0.0";
 const server = createServer();
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on:`);
+  console.log(`Local:   http://localhost:${PORT}`);
 });
